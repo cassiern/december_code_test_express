5aca85d17f01d53f26fa3d17b6a68a547f07398d
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2020-02-07 10:26:28 -0700

    changed CORS option to heroku react url,instead of localhost:3000

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 const corsOptions = {
-	origin: 'http://localhost:3000',
+	origin: 'https://travelers-pin-board.herokuapp.com/',
 	credentials: true,
 	optionsSuccessStatus: 200
 }

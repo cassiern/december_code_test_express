bbcd6b56755d604674d659d9dab81ff9b915610b
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2020-02-07 11:19:26 -0700

    commenting out mongoStore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app     = express();
 const bodyParser = require('body-parser');
 const cors    = require('cors');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+//const MongoStore = require('connect-mongo')(session);
 require('dotenv').config()
 
 require('./db/db');
